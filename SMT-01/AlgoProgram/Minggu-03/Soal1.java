8d6787795beef867f87d6b4216c7a0f680805b37
Wyze00 <144517602+Wyze00@users.noreply.github.com>
2024-10-03 16:57:11 +0700

    Create Soal1.java

---
+++ SMT-01/AlgoProgram/Minggu-03/Soal1.java
@@ -0,0 +1,19 @@
+import  java.util.Scanner;
+
+public class Soal1 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        float totalHarga = scan.nextFloat();
+
+        if(totalHarga > 100){
+
+            totalHarga = (float) (totalHarga * 0.9);
+
+        }
+
+        System.out.println(totalHarga);
+    }
+}
